1798858475b352febf51703a61617f4b9030e7c5
Nick Green <47676690+nickgreen43@users.noreply.github.com>
2019-02-19 11:26:16 -0600

    Update DisplayPIDigits.java

--- Number Projects/DisplayPIDigits.java
+++ Number Projects/DisplayPIDigits.java
@@ -7,24 +7,22 @@ public class TestEnvironment {
 
     public static void main(String[] args) {
 
-        BigDecimal pi = new BigDecimal("3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091");
-
         Scanner scan = new Scanner(System.in);
+        BigDecimal oneHundredThirteen = new BigDecimal(113.0);
 
-        System.out.println(
-                "Enter how many digits of pi to show: ");
-        int input = Integer.parseInt(scan.nextLine());
+    
+        System.out.println("Enter how many digits of pi aproximation to show: ");
+        int numberOfDecimalPlaces = Integer.parseInt(scan.nextLine());
 
-        while (input > 250) {
-            System.out.println("Too large. Enter a number less than 251!");
-            input = Integer.parseInt(scan.nextLine());
+        while (numberOfDecimalPlaces > 100) {
+            System.out.println("Please enter a number < 101");
+            numberOfDecimalPlaces = Integer.parseInt(scan.nextLine());
         }
 
-        String str = pi + "";
-        str = str.substring(0, input + 2);
-
-        System.out.println(str);
+        
+        BigDecimal pi = new BigDecimal(355.0).divide(oneHundredThirteen, numberOfDecimalPlaces, BigDecimal.ROUND_UP);
+    
+        System.out.println(pi);
 
     }
-
 }
